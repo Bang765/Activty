51a4fb38cd87516eadc99f2f7149eb723f1d5416
Toto_OG <toshithchalapathy@gmail.com>
2026-04-01 17:00:29 +0530

    Add files via upload

---
+++ printstatements.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, World!")
+
+# Printing my name
+print("my name is Alex")
+
+# Printing a message about my interests
+print("I love playing soccer")
+
+# performing and printing a simple calculation
+print("The sum of 5 and 3 is:", 5 + 3)
---
+++ variables.py
@@ -0,0 +1,11 @@
+h = 10
+t = 25
+print(h)
+print(t)
+print(h + t)
+
+
+name = "Toshith"
+print(name)
+
+print("Hello", name, "\nWelcome to Codingal!" )
